775c7f6013ffc06703c10677dec95fae5564f992
Carlos Mendes <cmendesce@gmail.com>
2020-05-22 18:14:53 -0300

    Update test.js

--- test.js
+++ test.js
@@ -3,27 +3,27 @@ import http from 'k6/http';
 export let options = {
     stages: [
 
-        { duration: "10s", target: 4 },
+        { duration: "20s", target: 10 },
 
-        { duration: "10s", target: 4 },
+        { duration: "20s", target: 10 },
 
-        { duration: "10s", target: 8 },
+        { duration: "20s", target: 20 },
 
-        { duration: "10s", target: 8 },
+        { duration: "20s", target: 20 },
 
-        { duration: "10s", target: 12 },
+        { duration: "20s", target: 40 },
 
-        { duration: "10s", target: 12 },
+        { duration: "20s", target: 40 },
 
-        { duration: "10s", target: 8 },
+        { duration: "20s", target: 20 },
 
-        { duration: "10s", target: 8 },
+        { duration: "20s", target: 20 },
 
-        { duration: "10s", target: 4 },
+        { duration: "20s", target: 10 },
 
-        { duration: "10s", target: 4 },
+        { duration: "20s", target: 10 },
 
-        { duration: "10s", target: 0 }
+        { duration: "20s", target: 0 }
     ]
 };
 
